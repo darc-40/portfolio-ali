5b54d34ca2448a4794e6aea49c1c2adff8a6700d
darc-40 <odhiamboalphonce23@gmail.com>
2023-05-24 19:01:46 +0300

    files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-3xl underline text-green-600'>mama</h1>
+      <h1>jhghg</h1>
+     
+    </div>
+  );
+}
+
+export default App;
